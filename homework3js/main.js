928c61b5619dfe94b87fddf3f15f75afc80cc71f
Sofi-Sh <sonia.sh13f@gmail.com>
2021-07-27 19:45:36 +0300

    main.js

---
+++ homework3js/main.js
@@ -0,0 +1,79 @@
+// - створити функцію яка обчислює та повертає площу прямокутника висотою
+// - створити функцію яка обчислює та повертає площу кола
+// - створити функцію яка обчислює та повертає площу циліндру
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+// - створити функцію яка  створює блок з текстом. Текст задати через аргумент
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+// function s1 (a,b){
+// return a*b;
+// }
+// let squere1=s1 (10,20);
+// console.log(squere1);
+//
+// function s2 (r){
+//     return Math.PI * Math.pow(r,2);
+// }
+// let squere2 = s2(10);
+// console.log(squere2);
+//
+// function s3 (r,H) {
+//     return 2* Math.PI * r * (r+H);
+// }
+// let squere3= s3 (10, 5);
+// console.log(squere3);
+
+
+// function f4 (){
+//     let max = arguments [0];
+//     let min = arguments [0];
+// for (let i = 0; i < arguments.length; i++) {
+//     if (arguments [i]>max){
+//         max=arguments[i];
+//     }
+//     if (arguments [i]>min){
+//         max=arguments[i];
+//     }
+// }
+// console.log(max);
+// return min;
+// }
+// console.log(f4(10,20,600,5678.7890));
+
+// function f5(textblock, text) {
+//     document.write(`<div> <h1> ${textblock} </h1>
+// <p> ${text} </p>
+// </div>`);
+// }
+//
+// f5('hello!', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+// f5 ('hi!', 'Accusamus aperiam asperiores culpa dolores, error libero magnam neque nihil non... ');
+// f5('HELLO OKTEN', 'asdqweasdqew');
+//
+
+// function f6 (txt) {
+//     document.write(`<ul>
+//  <li>${txt}</li>
+//  <li>${txt}</li>
+//  <li>${txt}</li>
+// </ul>`);
+//
+// }
+// f6 ('Lorem ipsum dolor sit amet, consectetur adipisicing elit.');
+
+//
+// function f7 (ulka, text){
+//     document.write("<ul>");
+//     for (let i=0; i <text; i++){
+//         document.write(`<li>${ulka}</li>`);
+//     }
+//     document.write("</ul>");
+// }
+// f7 ('Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 3);
+
+
+
+//???
+// /- створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
